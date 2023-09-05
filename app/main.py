19fee445435cff6366bf607e8080cad8a426107b
Yeon <112537146+yeon0306@users.noreply.github.com>
2023-09-05 14:47:37 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,14 @@
-from fastapi import FastAPI
+from typing import Optional, List
+
+from fastapi import FastAPI, Query
+from resolver import random_items, random_geners_items
+from recommender import item_based_recommendation,user_based_recommendation
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello 박혜연"}
-
-from resolver import random_items, random_geners_items
 
 @app.get("/all")
 async def all_movies():
@@ -18,11 +20,16 @@ async def genre_movies(genre: str):
     result = random_geners_items(genre)
     return {"result": result}
 
-from recommender import item_based_recommendation
-
 @app.get("/item-based/{item_id}")
 async def item_based(item_id: str):
     result = item_based_recommendation(item_id)
     return {"result": result}
 
+@app.get("/user-based/")
+async def user_based(params: Optional[List[str]] = Query(None)):
+    input_rating_dict = dict(
+        (int(x.split(":")[0]), float(x.split(":")[1])) for x in params
+    )
+    result = user_based_recommendation(input_rating_dict)
+    return {"result": result}
 
